4ba8e606ad09b8fdc7d1b0240db3f94c9448cf7d
BenVeghte <Benveghte@live.com>
2020-12-22 15:03:00 -0800

    Create teensy_and_icm-20948.ino

---
+++ Arduino/Main/teensy_and_icm-20948/teensy_and_icm-20948.ino
@@ -0,0 +1,25 @@
+/*
+ *  Project     Virtual Joystick 
+ *  @author     Ben Veghte
+ *  @link       github.com/BenVeghte/Virtual-Joystick
+ *  @license    GNU GPLv3
+ *  
+ *  This code is the first version of the virtual joystick project only using the IMU and 
+ *  the teensy 4.0. Teensyduino
+ *  
+ */
+
+/*********************REQUIRED LIBRARIES*********************/
+//The thing about teensys that took me a bit ti figure out, is the default libraries are added when the 
+
+/************************************************************/
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
